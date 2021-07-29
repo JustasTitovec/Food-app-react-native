2926edc801f8fc37817f940a6a6cd52b192ffce9
Justas Titovec <justas.titovec@yahoo.com>
2021-07-29 17:55:00 +0200

    rendering fetched items

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,27 +1,17 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
-import yelp from '../api/yelp';
+import useResults from '../hooks/useResults';
+import ResultsList from '../components/ResultsList';
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
-  const [results, setResults] = useState([]);
-  const [error, setError] = useState('');
+  const [searchApi, results, error] = useResults();
 
-  const searchApi = async (searchTerm) => {
-    try {
-      const response = await yelp.get('/search', {
-        params: {
-          limit: 50,
-          term: searchTerm,
-          location: 'san jose',
-        },
-      });
-
-      setResults(response.data.businesses);
-    } catch (err) {
-      setError('Something went wrong');
-    }
+  const filterResultsByPrice = (price) => {
+    return results.filter((result) => {
+      return result.price === price;
+    });
   };
 
   return (
@@ -36,6 +26,9 @@ const SearchScreen = () => {
       ) : (
         <Text>We have found {results.length}</Text>
       )}
+      <ResultsList results={filterResultsByPrice('$')} title="Cost Effective" />
+      <ResultsList results={filterResultsByPrice('$$')} title="Bit Pricier" />
+      <ResultsList results={filterResultsByPrice('$$$')} title="Big Spender!" />
     </View>
   );
 };
